39afee6ca1e1dc67234c35a5fe0ff5b1a6e23df2
Alexander Dadiani <alexdadiani1994@gmail.com>
2025-01-10 22:17:33 +0100

    copy timestep impl from win-loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
+use std::time::Duration;
+
 use anyhow::Result;
 use winit::event_loop::{ControlFlow, EventLoop};
 
-use self::window::StateApplication;
+use self::window::{Config, StateApplication};
 
 mod game;
 mod graphics;
 mod window;
 
 pub async fn run() -> Result<()> {
-    let mut app = StateApplication::new();
+    let config = Config::new(60, Duration::from_millis(17));
+    let mut app = StateApplication::new(config);
     let event_loop = EventLoop::new()?;
     event_loop.set_control_flow(ControlFlow::Poll);
     event_loop.run_app(&mut app).unwrap();
